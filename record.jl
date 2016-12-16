68bc96514ba77761c06b88e373a4ed52bdf65491
Shashi Gowda <shashigowda91@gmail.com>
2016-12-16 11:38:44 +0530

    Revert "use a subset of fields in a record"
    it's slow, it's prabably better to do it with a skipped field
    
    This reverts commit 9b8ada0f42647d5274101e14bce38a357d1fe5fc.

--- record.jl
+++ record.jl
@@ -1,23 +1,14 @@
-immutable Record{Tf<:Tuple, To,N}
+immutable Record{Tf<:Tuple, To}
     fields::Tf
-    use::NTuple{N,Int}
 end
 
-function Record{T<:Tuple}(t::T, use=())
-    To = Tuple{subtuple(Any, map(fieldtype, t), use)...}
+function Record{T<:Tuple}(t::T)
+    To = Tuple{map(fieldtype, t)...}
     #Tov = Tuple{map(s->Vector{s},map(fieldtype, t))...}
-    Record{T, To, length(use)}(t, use)
+    Record{T, To}(t)
 end
 
-@inline function subtuple{N}(T, t, idx::NTuple{N})
-    ntuple(i -> t[idx[i]], Val{N})::T
-end
-
-@inline function subtuple(T, t, idx::Tuple{})
-    t::T
-end
-
-@generated function tryparsenext{N,To,M}(r::Record{NTuple{N},To,M}, str, i, len)
+@generated function tryparsenext{N,To}(r::Record{NTuple{N},To}, str, i, len)
     quote
         R = Nullable{To}
         i > len && @goto error
@@ -26,29 +17,22 @@ end
             @chk2 (val_j, i) = tryparsenext(r.fields[j], str, i, len)
         end
 
-        res = subtuple(To, (Base.@ntuple $N val), r.use)
-
         @label done
-        return R(res), i
+        return R(Base.@ntuple $N val), i
 
         @label error
         R(), i
     end
 end
 
-@generated function tryparsesetindex{N,To,M}(r::Record{NTuple{N},To,M}, str, i, len, columns::NTuple{M}, row)
+@generated function tryparsesetindex{N,To}(r::Record{NTuple{N},To}, str, i, len, columns::Tuple, col)
     quote
         R = Nullable{Void}
         i > len && @goto error
 
         Base.@nexprs $N j->begin
             @chk2 val_j, i = tryparsenext(r.fields[j], str, i, len)
-        end
-
-        res = subtuple(To, (Base.@ntuple $N val), r.use)
-
-        Base.@nexprs $M j->begin
-            columns[j][row] = res[j]
+            columns[j][col] = val_j
         end
 
         @label done
@@ -68,8 +52,5 @@ let
     @test tryparsenext(r, "12,21.0,21,", 1, 9) |> failedat == 6
     s = "12   ,  21,  21.23,"
     @test tryparsenext(r, s, 1, length(s)) |> unwrap == ((12, UInt(21), 21.23), length(s)+1)
-
-    r2 = Record(r.fields, (1,3))
-    @test tryparsenext(r2, "12,21,21,", 1, 9) |> unwrap == ((12, 21.0), 10)
     nothing
 end
