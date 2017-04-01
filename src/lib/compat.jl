18c060316e4aea6374a622761fbdc07875e0fa2d
Tony Kelman <tony@kelman.net>
2017-04-01 04:33:34 -0700

    use isdefined for definition of datatype_name

--- src/lib/compat.jl
+++ src/lib/compat.jl
@@ -1,5 +1,10 @@
-if VERSION < v"0.6.0-dev"
+if isdefined(Base, :datatype_name)
+    datatype_name(x) = Base.datatype_name(x)
+else
     datatype_name(t) = t.name.name
+end
+
+if VERSION < v"0.6.0-dev"
     include("fast-dates.jl")
 
     include("Str.jl")
@@ -7,7 +12,6 @@ else
     const Str = String
     import Base.Dates: CONVERSION_SPECIFIERS, TimeType, DatePart, tryparsenext, character_codes, genvar, CONVERSION_TRANSLATIONS, CONVERSION_DEFAULTS, _directives, DateLocale
 
-    datatype_name(x) = Base.datatype_name(x)
     include("date-tryparse-internal.jl")
     const ISODateFormat = Base.Dates.ISODateFormat
     const ISODateTimeFormat = Base.Dates.ISODateTimeFormat
