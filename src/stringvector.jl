4c52abc6f1dd369f078160a67472abc17b53dc02
Shashi Gowda <shashi@juliacomputing.com>
2018-03-13 18:45:16 +0530

    always keep lengths updated

--- src/stringvector.jl
+++ src/stringvector.jl
@@ -20,9 +20,7 @@ function StringVector(pa::PooledArray{<:AbstractString})
             push!(res, pa.pool[pa.refs[i]])
         else
             push!(res.offsets, UNDEF_OFFSET)
-            if !isempty(res.lengths)
-                push!(res.lengths, 0)
-            end
+            push!(res.lengths, 0)
         end
     end
     return res
@@ -63,9 +61,6 @@ Base.copy(a::StringVector) = StringVector(copy(a.buffer), copy(a.offsets), copy(
     p = pointer(arr.buffer)
     if val.ptr <= p + length(arr.buffer)-1 && val.ptr >= p
         # this means we know this guy.
-        if isempty(arr.lengths)
-            fill_lengths!(arr)
-        end
         arr.offsets[idx] = val.ptr - p
         arr.lengths[idx] = val.len
     else
@@ -77,49 +72,14 @@ end
     _setindex!(arr, val, idx)
 end
 
-function fill_lengths!(arr::StringVector)
-    resize!(arr.lengths, length(arr))
-    offsets = arr.offsets
-    # fill lengths array
-    for i=1:length(offsets)-1
-        next_o = offsets[i+1]
-        if next_o == UNDEF_OFFSET
-            # fill_lengths! is being called for the first time
-            # an offset is UNDEF_OFFSET means that this is the
-            # last element in the array filled so far.
-            next_o = UInt64(length(arr.buffer))
-        end
-        arr.lengths[i] = next_o - ifelse(offsets[i]==UNDEF_OFFSET, next_o, offsets[i])
-    end
-    if offsets[end] !== UNDEF_OFFSET
-        arr.lengths[end] = length(arr.buffer) - offsets[end]
-    else
-        arr.lengths[end] = 0
-    end
-end
-
 function _setindex!(arr::StringVector, val::AbstractString, idx::Real)
     buffer = arr.buffer
     l = length(arr.buffer)
-    if idx == length(arr) && isempty(arr.lengths) # set last element
-        resize!(buffer, arr.offsets[idx] + sizeof(val))
-        unsafe_copy!(pointer(buffer, l+1), pointer(val,1), sizeof(val))
-        arr.offsets[idx] = idx > 1 ? arr.offsets[idx-1] + sizeof(val) : sizeof(val)
-        if !isempty(arr.lengths)
-            arr.lengths[idx] = sizeof(val)
-        end
-        val
-    else
-        # append to buffer
-        if isempty(arr.lengths)
-            fill_lengths!(arr)
-        end
-        resize!(buffer, l + sizeof(val))
-        unsafe_copy!(pointer(buffer, l+1), pointer(val,1), sizeof(val))
-        arr.lengths[idx] = sizeof(val)
-        arr.offsets[idx] = l
-        val
-    end
+    resize!(buffer, l + sizeof(val))
+    unsafe_copy!(pointer(buffer, l+1), pointer(val,1), sizeof(val))
+    arr.lengths[idx] = sizeof(val)
+    arr.offsets[idx] = l
+    val
 end
 
 function _setindex!(arr::StringVector, val::Union{StringVector,AbstractVector{<:AbstractString}}, idx::AbstractVector)
@@ -135,14 +95,10 @@ end
 function Base.resize!(arr::StringVector, len)
     l = length(arr)
     resize!(arr.offsets, len)
-    if !isempty(arr.lengths)
-        resize!(arr.lengths, len)
-    end
+    resize!(arr.lengths, len)
     if l < len
         arr.offsets[l+1:len] = UNDEF_OFFSET # undef
-        if !isempty(arr.lengths)
-            arr.lengths[l+1:len] = 0
-        end
+        arr.lengths[l+1:len] = 0
     end
     arr
 end
@@ -152,8 +108,6 @@ function Base.push!(arr::StringVector, val::AbstractString)
     resize!(arr.buffer, l + sizeof(val))
     unsafe_copy!(pointer(arr.buffer, l + 1), pointer(val,1), sizeof(val))
     push!(arr.offsets, l)
-    if !isempty(arr.lengths)
-        push!(arr.lengths, sizeof(val))
-    end
+    push!(arr.lengths, sizeof(val))
     arr
 end
